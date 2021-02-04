5e2f2d04a45441c351b72a0163049139bb3c73b0
Trent Lowe <trentrlowe@gmail.com>
2021-02-04 01:39:21 -0500

    ignore

---
+++ script.js
@@ -0,0 +1,8 @@
+function loadWelcome() {
+    setTimeout(welcomeText, 3000);
+  }
+  
+  function welcomeText() {
+    document.getElementById("welcome").innerHTML = "welcome to our university.";
+  }
+  
